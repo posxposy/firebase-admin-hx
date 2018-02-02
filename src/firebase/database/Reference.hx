7ad75d051fd21742399be57ead2c8449099b3ec2
nikolay.tkachenko <d2rk87@gmail.com>
2018-02-02 11:54:13 +0200

    added update prototype

--- src/firebase/database/Reference.hx
+++ src/firebase/database/Reference.hx
@@ -13,10 +13,11 @@ extern class Reference extends Query
     public var parent:Reference;
     public var root:Reference;
     
-    public function child(path:String):Reference;
-    
+    public function child(path:String):Reference;	
+
     public function push(?value:Dynamic, ?onComplete:Dynamic->Void):ThenableReference;
     public function set(value:Dynamic, ?onComplete:Dynamic->Void):Promise<Void>;
     public function transaction(transactionUpdate:Dynamic->Void, ?onComplete:Dynamic->Bool->DataSnapshot->Void, ?applyLocally:Bool):Promise<{committed:Bool, snapshot: DataSnapshot}>;
     public function remove(?onComplete:Dynamic->Void):Promise<Dynamic>;
+    public function update(?value:Dynamic, ?onComplete:Dynamic->Void):Promise<Void>;
 }
