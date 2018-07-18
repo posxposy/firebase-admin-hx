b3b83b32876683aa29026ad529e6249f049dc944
Dmitry Hryppa <dmitry.hryppa@gmail.com>
2018-07-19 02:51:08 +0300

    Add missing methods

--- src/firebase/auth/Auth.hx
+++ src/firebase/auth/Auth.hx
@@ -2,7 +2,6 @@ package firebase.auth;
 import firebase.auth.CreateRequest;
 import js.Promise;
 
-
 @:native('admin.auth.Auth') 
 extern class Auth {
     public function verifyIdToken(idToken:String):Promise<DecodedIdToken>;
@@ -10,4 +9,6 @@ extern class Auth {
     public function createUser(parameters:CreateRequest):Promise<UserRecord>;
     public function getUserByEmail(email:String):Promise<UserRecord>;
     public function getUserByPhoneNumber(phoneNumber:String):Promise<UserRecord>;
+    public function deleteUser(uid:String):Promise<Void>;
+    public function createCustomToken(uid:String, ?additionalClaims:Dynamic):Promise<String>;
 }
