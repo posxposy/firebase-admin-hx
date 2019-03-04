28dc318564c882940e20d3e0d53a6c1e306f25a8
Dmitry Hryppa <dmitry.hryppa@gmail.com>
2019-03-04 04:54:17 +0200

    add UpdateRequest

--- src/firebase/auth/Auth.hx
+++ src/firebase/auth/Auth.hx
@@ -8,7 +8,7 @@ extern class Auth {
 	public function verifyIdToken(idToken:String):Promise<DecodedIdToken>;
 	public function getUser(uid:String):Promise<UserRecord>;
 	public function createUser(parameters:CreateRequest):Promise<UserRecord>;
-	public function updateUser(uid:String, parameters:CreateRequest):Promise<UserRecord>;
+	public function updateUser(uid:String, parameters:UpdateRequest):Promise<UserRecord>;
 	public function getUserByEmail(email:String):Promise<UserRecord>;
 	public function getUserByPhoneNumber(phoneNumber:String):Promise<UserRecord>;
 	public function deleteUser(uid:String):Promise<Void>;
---
+++ src/firebase/auth/UpdateRequest.hx
@@ -0,0 +1,11 @@
+package firebase.auth;
+
+typedef UpdateRequest = {
+	?disabled:Bool,
+	?displayName:String,
+	?email:String,
+	?emailVerified:Bool,
+	?password:String,
+	?phoneNumber:String,
+	?photoURL:String
+}
